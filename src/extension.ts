f3bc162599274a16e614d87a6d88fb090693ec75
jpalioto <john.p.alioto@gmail.com>
2015-12-02 11:57:44 -0800

    Unit-Tests

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'; 
+import * as bll from './bll';
 
 // Open will bring up the default browser and navigate to a URI.
-var open = require("open");
+var open = require( "open" );
 
-const timerPeriod = 3600000; 					// The peroidicity of the message pop-up in miliseoncds.
+const timerPeriod = 2000; 					// The peroidicity of the message pop-up in miliseoncds.
 const showMeButtonText = 'Show Me'; 			// This text will be shown on the button that brings up the browser
 const stopButtonText = 'Stop Alerts';			// This text will be on the button that stops the messages from showing.
 
@@ -15,16 +16,16 @@ const imageTargetUrl = 'http://bit.ly/1TsGOW5';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate( context: vscode.ExtensionContext ) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "Stretch Timer" is now active!  See you in an hour!'); 
+	console.log( 'Congratulations, your extension "Stretch Timer" is now active!  See you in an hour!' ); 
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.stretchTimer', () => {
+	var disposable = vscode.commands.registerCommand( 'extension.stretchTimer', () => {
 		// Set the timer on a recurring basis.
 		var timer = setInterval( 
 			() => {
@@ -36,7 +37,7 @@ export function activate(context: vscode.ExtensionContext) {
 		 );
 	});
 	
-	context.subscriptions.push(disposable);
+	context.subscriptions.push( disposable );
 }
 
 // This method gets called when the timer goes off
@@ -44,9 +45,9 @@ export function activate(context: vscode.ExtensionContext) {
 export function TimerCallback( timer )
 {
 	// Display a information message.
-	vscode.window.showInformationMessage(GetAlertText(), 
+	vscode.window.showInformationMessage( bll.GetAlertText( timerPeriod ), 
 										stopButtonText,
-										showMeButtonText)
+										showMeButtonText )
 		.then( (b)=> {
 						// This is the close button ugh.
 						if( typeof b == 'undefined' )
@@ -55,38 +56,12 @@ export function TimerCallback( timer )
 						} 
 						else if( b == showMeButtonText )
 						{
-							ShowImage( imageTargetUrl );
+							bll.ShowImage( imageTargetUrl );
 						} 
 						else if( b == stopButtonText )
 						{
-							StopTimer( timer );	
+							bll.StopTimer( timer );	
 						}
 					 } );
 		
-}
-
-// This is the message that we show ever X seconds.  
-// TODO: Internationalize
-export function GetAlertText()
-{
-	// The period is in seconds.  We want minutes;
-	var minutes = ToNearestMinute(timerPeriod);
-	return 'You have been inactive for ' + minutes + ' minutes. Time to stretch!'
-}
-
-// Call this function when you want to stop a timer.
-export function StopTimer( timer )
-{
-	clearInterval(timer);
-}
-
-// Bring up the browser to the target URL.
-export function ShowImage( targetUrl )
-{
-	open(targetUrl);
-}
-
-export function ToNearestMinute( miliseconds )
-{
-	return Math.trunc( miliseconds/1000/60 );
 }
--- test/extension.test.ts
+++ test/extension.test.ts
@@ -5,18 +5,21 @@
 
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
+import * as bll from '../src/bll';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
 
+import expect = require('expect')
+
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+suite("Business Logic Tests", () => {
 
 	// Defines a Mocha unit test
-	test("Something 1", () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
-	});
+	test("Test ToNearestMinute", () => {
+		assert( bll.ToNearestMinute(3600000), '60' );
+		expect( () => { bll.ToNearestMinute(-1); } ).toThrow( 'Invalid Argument less than 0 sent to ToNearestMinute' ); 
+	});	
 });
